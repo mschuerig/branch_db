d5128923fcb705bbc37ca55e2a01da352827390a
Michael Schuerig <michael@schuerig.de>
2009-05-25 10:42:44 +0200

    DB copy: Show full database name.

--- lib/branch_db/switcher.rb
+++ lib/branch_db/switcher.rb
@@ -69,7 +69,7 @@ module BranchDb # :nodoc:
       ensure_branch_db_exists!(from_branch)
 
       create_empty_database do
-        puts "Copying data from #{from_branch}..."
+        puts "Copying data from #{branch_db(from_branch)}..."
         copy_database(from_branch, @branch)
       end
     end
